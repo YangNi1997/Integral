54ac2b04fac7bc611965f1d82494731c9363f92c
Yang Ni <970085090@qq.com>
2020-07-07 17:35:28 +0800

    f(x)的定义在类外面。

--- Python Integral/Gauss.py
+++ Python Integral/Gauss.py
@@ -6,15 +6,23 @@ class GaussLegendreQuadrature:
         self.b=b_1
         self.legendre_epsilon=np.array([-0.9061798459386641,-0.538469310105683,-1.081853856991421e-16,0.5384693101056831,0.9061798459386639])
         self.legendre_w=np.array([0.2369268850561892,0.4786286704993669,0.568888888888889,0.4786286704993672,0.2369268850561891])
-    def func(self,x):
-        fvalue=x*2
-        return fvalue
 
-    def integral(self):
+    def callFunction(self,funcName,a):
+        return funcName(a)
+
+    def integral(self,func):
         x_array=self.legendre_epsilon*(self.b-self.a)/2+(self.a+self.b)/2
-        fvalue_array=self.func(x_array)
+        fvalue_array=self.callFunction(func,x_array)
         quadrature=(self.b-self.a)/2*np.sum(self.legendre_w*fvalue_array)
         print(quadrature)
 
-test1=GaussLegendreQuadrature(3,6)
-test1.integral()
+def func1(x):
+    function=x*2
+    return function
+
+def func2(x):
+    function=x*3
+    return function
+
+test1=GaussLegendreQuadrature(2,4)
+test1.integral(func2)
