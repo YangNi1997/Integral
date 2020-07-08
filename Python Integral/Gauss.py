9555ece48a5157b0b296164afd6a7f222a649ba3
Yang Ni <970085090@qq.com>
2020-07-08 09:06:43 +0800

    delete callFunction

--- Python Integral/Gauss.py
+++ Python Integral/Gauss.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 
 class GaussLegendreQuadrature:
@@ -7,12 +8,9 @@ class GaussLegendreQuadrature:
         self.legendre_epsilon=np.array([-0.9061798459386641,-0.538469310105683,-1.081853856991421e-16,0.5384693101056831,0.9061798459386639])
         self.legendre_w=np.array([0.2369268850561892,0.4786286704993669,0.568888888888889,0.4786286704993672,0.2369268850561891])
 
-    def callFunction(self,funcName,a):
-        return funcName(a)
-
     def integral(self,func):
         x_array=self.legendre_epsilon*(self.b-self.a)/2+(self.a+self.b)/2
-        fvalue_array=self.callFunction(func,x_array)
+        fvalue_array=func(x_array)
         quadrature=(self.b-self.a)/2*np.sum(self.legendre_w*fvalue_array)
         print(quadrature)
 
@@ -25,4 +23,4 @@ def func2(x):
     return function
 
 test1=GaussLegendreQuadrature(2,4)
-test1.integral(func2)
+test1.integral(func1)
