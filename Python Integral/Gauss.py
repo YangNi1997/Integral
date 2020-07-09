a11dbffcc24b2e90427573d513e549df2a868e74
Yang Ni <970085090@qq.com>
2020-07-09 15:09:59 +0800

    BaseQuadrature is used to define GaussLegendreQuadrature.

--- Python Integral/Gauss.py
+++ Python Integral/Gauss.py
@@ -1,26 +1,20 @@
-
+#!/usr/bin/env python3
 import numpy as np
+from Base_Class import BaseQuadrature
 
-class GaussLegendreQuadrature:
+class GaussLegendreQuadrature(BaseQuadrature):
     def __init__(self,a_1,b_1):
-        self.a=a_1
-        self.b=b_1
+        super().__init__(a_1,b_1)
         self.legendre_epsilon=np.array([-0.9061798459386641,-0.538469310105683,-1.081853856991421e-16,0.5384693101056831,0.9061798459386639])
         self.legendre_w=np.array([0.2369268850561892,0.4786286704993669,0.568888888888889,0.4786286704993672,0.2369268850561891])
-
-    def integral(self,func):
-        x_array=self.legendre_epsilon*(self.b-self.a)/2+(self.a+self.b)/2
-        fvalue_array=func(x_array)
-        quadrature=(self.b-self.a)/2*np.sum(self.legendre_w*fvalue_array)
-        print(quadrature)
 
 def func1(x):
     function=x*2
     return function
 
 def func2(x):
-    function=x*4
+    function=x
     return function
 
-test1=GaussLegendreQuadrature(2,4)
-test1.integral(func1)
+test1=GaussLegendreQuadrature(1,8)
+test1.integral(func2)
