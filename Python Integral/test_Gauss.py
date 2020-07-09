03c2b00ab833804db1268a8d940b9900ab58b384
Yang Ni <970085090@qq.com>
2020-07-09 15:15:33 +0800

    修改了断言中的参数

--- Python Integral/test_Gauss.py
+++ Python Integral/test_Gauss.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python3
 import unittest
 from Gauss import GaussLegendreQuadrature
 
 class TestGauss(unittest.TestCase):
     def test_function1(self):
+        a=2
+        b=6
         test1=GaussLegendreQuadrature(2,6)
-        self.assertAlmostEqual(16.0,test1.integral(func1))
+        self.assertAlmostEqual((b*b-a*a)/2,test1.integral(func1))
 
     def test_function2(self):
+        a=1
+        b=8
         test2=GaussLegendreQuadrature(1,8)
-        self.assertAlmostEqual(94.5,test2.integral(func2))
+        self.assertAlmostEqual((b*b-a*a)*1.5,test2.integral(func2))
 
 def func1(x):
     function=x
