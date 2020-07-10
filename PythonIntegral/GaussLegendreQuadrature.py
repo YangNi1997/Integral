4ee0883af05c5911993fe6502fb0c2ed23979e11
Yang Ni <970085090@qq.com>
2020-07-10 09:05:36 +0800

    change names of parameters in init, import Function

--- PythonIntegral/GaussLegendreQuadrature.py
+++ PythonIntegral/GaussLegendreQuadrature.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
+import Function
 from BaseQuadrature import BaseQuadrature
 
 class GaussLegendreQuadrature(BaseQuadrature):
-    def __init__(self,a_1,b_1):
-        super().__init__(a_1,b_1)
-        self.legendre_epsilon=np.array([-0.9061798459386641,-0.538469310105683,-1.081853856991421e-16,0.5384693101056831,0.9061798459386639])
+    def __init__(self,min,max):
+        super().__init__(min,max)
+        self.legendre_x=np.array([-0.9061798459386641,-0.538469310105683,-1.081853856991421e-16,0.5384693101056831,0.9061798459386639])
         self.legendre_w=np.array([0.2369268850561892,0.4786286704993669,0.568888888888889,0.4786286704993672,0.2369268850561891])
 
-def func1(x):
-    function=x*2
-    return function
-
-def func2(x):
-    function=x
-    return function
-
 test1=GaussLegendreQuadrature(1,8)
-test1.integral(func2)
+integral_result=test1.integral(Function.mutiply_one)
+print(integral_result)
