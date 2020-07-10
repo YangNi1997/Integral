e9343ec7e91f50d33d60207c817bae89714829ed
Yang Ni <970085090@qq.com>
2020-07-10 09:05:56 +0800

    change names of parameters in init, import Function

--- PythonIntegral/NewtonCotesQuadrature.py
+++ PythonIntegral/NewtonCotesQuadrature.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
+import Function
 from BaseQuadrature import BaseQuadrature
 
 class NewtonCotesQuadrature(BaseQuadrature):
-    def __init__(self,a_1,b_1):
-        super().__init__(a_1,b_1)
-        self.legendre_epsilon=np.array([-1.00000000000000000000,-0.50000000000000000000,0.00000000000000000000,0.50000000000000000000,1.00000000000000000000])
+    def __init__(self,min,max):
+        super().__init__(min,max)
+        self.legendre_x=np.array([-1.00000000000000000000,-0.50000000000000000000,0.00000000000000000000,0.50000000000000000000,1.00000000000000000000])
         self.legendre_w=np.array([0.15555555555555555556,0.71111111111111111111,0.26666666666666666667,0.71111111111111111111,0.15555555555555555556])
 
-def func1(x):
-    function=x
-    return function
-
-def func2(x):
-    function=x*4
-    return function
-
 test2=NewtonCotesQuadrature(2,8)
-test2.integral(func1)
+integral_result=test2.integral(Function.multiply_two)
+print(integral_result)
