0a37daf44a64fd0ce29fe6a61b0766b471e30dbe
Yang Ni <970085090@qq.com>
2020-07-13 15:15:47 +0800

    delete object test1

--- PythonIntegral/GaussLegendreQuadrature.py
+++ PythonIntegral/GaussLegendreQuadrature.py
@@ -6,9 +6,5 @@ from BaseQuadrature import BaseQuadrature
 class GaussLegendreQuadrature(BaseQuadrature):
     def __init__(self,min,max):
         super().__init__(min,max)
-        self.legendre_x=np.array([-0.9061798459386641,-0.538469310105683,-1.081853856991421e-16,0.5384693101056831,0.9061798459386639])
-        self.legendre_w=np.array([0.2369268850561892,0.4786286704993669,0.568888888888889,0.4786286704993672,0.2369268850561891])
-
-test1=GaussLegendreQuadrature(1,8)
-integral_result=test1.integral(Function.mutiply_one)
-print(integral_result)
+        self.abscissa_x=np.array([-0.9061798459386641,-0.538469310105683,-1.081853856991421e-16,0.5384693101056831,0.9061798459386639])
+        self.weight_w=np.array([0.2369268850561892,0.4786286704993669,0.568888888888889,0.4786286704993672,0.2369268850561891])
