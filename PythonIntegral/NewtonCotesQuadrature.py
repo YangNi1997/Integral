31b45b0b4994edb7af941acb06a247187734525d
Yang Ni <970085090@qq.com>
2020-07-15 16:57:15 +0800

    read DataNewtonCotes.h5 to get abscissa_x and weight_w

--- PythonIntegral/NewtonCotesQuadrature.py
+++ PythonIntegral/NewtonCotesQuadrature.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
+import h5py
 import Function
 from BaseQuadrature import BaseQuadrature
 
 class NewtonCotesQuadrature(BaseQuadrature):
     def __init__(self,min,max):
         super().__init__(min,max)
-        self.abscissa_x=np.array([-1.00000000000000000000,-0.50000000000000000000,0.00000000000000000000,0.50000000000000000000,1.00000000000000000000])
-        self.weight_w=np.array([0.15555555555555555556,0.71111111111111111111,0.26666666666666666667,0.71111111111111111111,0.15555555555555555556])
+        data=h5py.File('DataNewtonCotes.h5','r')
+        newtoncotes_x=data.get('newtoncotes/abscissa_x')
+        newtoncotes_w=data.get('newtoncotes/weight_w')
+        self.abscissa_x=np.array(newtoncotes_x)
+        self.weight_w=np.array(newtoncotes_w)
