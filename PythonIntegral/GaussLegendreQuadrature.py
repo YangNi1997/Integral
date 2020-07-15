4416f5b4bcdb152b17f16ebb7b79e5e19c63a42e
Yang Ni <970085090@qq.com>
2020-07-15 16:56:34 +0800

    read DataLegendre.h5 to get abscissa_x and weight_w

--- PythonIntegral/GaussLegendreQuadrature.py
+++ PythonIntegral/GaussLegendreQuadrature.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import numpy as np
+import h5py
 import Function
 from BaseQuadrature import BaseQuadrature
 
 class GaussLegendreQuadrature(BaseQuadrature):
     def __init__(self,min,max):
         super().__init__(min,max)
-        self.abscissa_x=np.array([-0.9061798459386641,-0.538469310105683,-1.081853856991421e-16,0.5384693101056831,0.9061798459386639])
-        self.weight_w=np.array([0.2369268850561892,0.4786286704993669,0.568888888888889,0.4786286704993672,0.2369268850561891])
+        data=h5py.File('DataLegendre.h5','r')
+        legendre_x=data.get('legendre/abscissa_x')
+        legendre_w=data.get('legendre/weight_w')
+        self.abscissa_x=np.array(legendre_x)
+        self.weight_w=np.array(legendre_w)
